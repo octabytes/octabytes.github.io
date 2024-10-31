3e8a6e3c11f4c1df57718624ba296f4c965889d4
AxeemHaider <axeemhaider@gmail.com>
2024-10-31 10:14:22 +0500

    update

--- extractor/services_json_to_md.py
+++ extractor/services_json_to_md.py
@@ -1,9 +1,9 @@
 import json
 
 # File paths
-categories_file = "./categories/hosting-and-infrastructure.json"
-services_file = "./services/hosting-and-infrastructure.json"
-output_md_file = "../content/hosting-and-infrastructure.md"
+categories_file = "./categories/databases.json"
+services_file = "./services/databases.json"
+output_md_file = "./output/databases.md"
 
 # Load JSON data
 with open(categories_file, "r") as f:
@@ -15,13 +15,13 @@ with open(services_file, "r") as f:
 # Convert category and service data into Markdown format
 def json_to_markdown(categories, services):
     md_content = """---
-title: "Hosting and Infrastructure"
+title: "Databases"
 draft: false
 layout: "services"
 
 content:
-  id: "hosting-and-infrastructure"
-  title: "'Hosting and Infrastructure'"
+  id: "databases"
+  title: "'Databases'"
   description: REPLACE_THIS
   categories:
 """
@@ -36,12 +36,18 @@ content:
     # Add service information
     for service in services:
         category_ids = " ".join(service["category"]["ids"])
+        
+        # Wrap description in quotes if it contains a colon
+        description = service['description']
+        if ':' in description:
+            description = f'"{description}"'
+        
         md_content += f"    - id: {service['id']}\n"
         md_content += f"      title: {service['title']}\n"
         md_content += f"      image: {service['logo']}\n"
         md_content += f"      url: {service['link']}\n"
         md_content += f"      category: {category_ids}\n"
-        md_content += f"      description: {service['description']}\n"
+        md_content += f"      description: {description}\n"
 
     md_content += "---"
     return md_content
